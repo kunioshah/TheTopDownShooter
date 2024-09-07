06314fa0563082cbc32f0aa374be4298c49590a8
kunioshah <kunioshah@gmail.com>
2024-09-07 04:11:28 -0700

    dont update score if enemy dies
    realized this when making the video oopsyy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -52,6 +52,7 @@ public class Enemy : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player") {
+            myManager.numOfEnemiesDefeated--;
             Die();
         }
 
